f41cbab7c82f70b23ba32fe0a54cba8eb16d56e0
Brodie Peberdy <brodie.peberdy@gmail.com>
2021-05-15 18:55:54 +0100

    Tidied up the styling for React, added some more UI stuff, license info.

--- parliament/src/index.js
+++ parliament/src/index.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styling.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 class LandingPage extends React.Component {
-  constructor() {
-    super();
-  }
   render() {
     return (
       <div>
-        <Header/>
-        <br/>
-        <Button text="House of Commons"/>
-        <br/>
-        <Search/>
+        <div>
+          <Header/>
+        </div>
+        <div>
+          <Buttons/>
+          <Search/>
+        </div>
+        <Footer/>
       </div>
     )
   }
 }
 
 class Header extends React.Component {
-  render() { 
-    const mystyle = {
-      position: "absolute",
-      textAlign: 'center',
-      fontWeight: 'bold',
-      top: 0,
-      right: 0,
-      left: 0,
-      margin: 0,
-      padding: 0,
-      color: "white",
-      backgroundColor: "SlateBlue",
-      padding: "10px"
-    };
-    return <h1 style={mystyle}>UK Parliament: Who Represents Me?</h1>;
+  render() {
+    return <h1 className="header">UK Parliament: Who Represents Me?</h1>;
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>Parliamentary information licensed under the <a href="https://www.parliament.uk/site-information/copyright-parliament/open-parliament-licence/">Open Parliament License v3.0</a>.</p>
+      </div>
+    );
   }
 }
 
@@ -42,12 +40,15 @@ class Search extends React.Component {
   render() {
     const mystyle = {
       textAlign: "center",
-      width: "100%"
+      width: "95%"
     };
     return (
-      <form>
-        <input style={mystyle} type="text"/>
-      </form>
+      <div className="search-container">
+        <form>
+          <input style={mystyle} type="text" placeholder="Search..."/>
+          <button type="submit"><i className="fa fa-search"></i></button>
+        </form>
+      </div>
     );
   }
 }
@@ -56,14 +57,41 @@ class Button extends React.Component {
   constructor(props) {
     super(props);
   }
+  select(type){
+    console.log(type);
+  }
   render() {
-    return <button type="button">{this.props.text}</button>;
+    const mystyle = {
+      textAlign: "center",
+      backgroundColor: this.props.background,
+      color: "White",
+      display: "block",
+      border: "none",
+      width: "25%",
+      padding: "1em",
+      margin: "0 auto",
+      display: "inline",
+      display: "inline-block",
+      cursor: "pointer"
+    };
+    return <button type="button" onClick={() => this.select(this.props.type)} style={mystyle}><i className={this.props.icon}></i>{this.props.text}</button>;
+  }
+}
+
+class Buttons extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <Button text=" House of Commons" type="HoC" icon="fa fa-bank" background="FireBrick"/>
+        <Button text=" House of Lords" type="HoL" icon="fa fa-bank" background="ForestGreen"/>
+        <Button text=" Parliamentary Bills" type="PB" icon="fa fa-book" background="Indigo"/>
+        <Button text=" Parliamentary Committees" type="PC" icon="fa fa-users" background="SteelBlue"/>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(<LandingPage/>, document.getElementById('root'));
-
-
-
-// const myelement = <h1>React is {5 + 5} times better with JSX</h1>;
-// ReactDOM.render(myelement, document.getElementById('root'));
